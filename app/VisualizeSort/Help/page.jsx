0263256af6be8cd5dc7866adbe3722ad570d8327
Prakhar <rampalliprakhar@gmail.com>
2025-03-09 13:28:59 -0400

    Fixed Minor Errors

--- app/VisualizeSort/Help/page.jsx
+++ app/VisualizeSort/Help/page.jsx
@@ -48,9 +48,9 @@ export default function HelpPage () {
 
         {/* Link to go back to the visualizer */}
         <div className="flex justify-center mb-4">
-        <Link href="/" legacyBehavior className='"bg-blue-600 text-white py-2 px-6 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 transition"'>
-          Back to Visualizer
-        </Link>
+          <Link href="/" className="bg-blue-600 text-white py-2 px-6 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 transition">
+            Back to Visualizer
+          </Link>
         </div>
       </div>
     </div>
--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,19 @@ import '../styles/globals.css';
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Navbar/>
-
-      <main>{children}</main>
-
-      <footer className="bg-gray-800 text-white text-center p-4">
-        <p>&copy; 2025 Sorting Visualizer. All rights reserved.</p>
-      </footer>
-    </div>
+    <html lang="en">
+      <head>
+        <meta charSet='UTF-8'/>
+        <meta name='viewport' content='width=device-width, initial-scale=1'/>
+        <title>Sorting Visualizer</title>
+      </head>
+      <body>
+        <Navbar/>
+        <main>{children}</main>
+        <footer className="bg-gray-800 text-white text-center p-4">
+          <p>&copy; 2025 Sorting Visualizer. All rights reserved.</p>
+        </footer>
+      </body>
+    </html>
   );
 }
